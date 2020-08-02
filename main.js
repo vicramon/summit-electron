7f2ab6ed3aa137fdce1a94589330df6f44fe31d4
Vic Ramon <v@vicramon.com>
2020-08-01 18:16:08 -0700

    Clean up code; make closing and reopening work

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const electron = require("electron");
-
 // failing – requires signature
 // require('update-electron-app')({
 //   repo: 'vicramon/summit-electron',
@@ -9,79 +8,6 @@ const { app, BrowserWindow } = electron;
 
 const path = require("path");
 const fs = require("fs");
-
-// Menu (for standard keyboard shortcuts)
-const { Menu } = require("electron");
-
-const template = [
-  {
-    label: "Edit",
-    submenu: [
-      { role: "undo" },
-      { role: "redo" },
-      { type: "separator" },
-      { role: "cut" },
-      { role: "copy" },
-      { role: "paste" },
-      { role: "pasteandmatchstyle" },
-      { role: "delete" },
-      { role: "selectall" },
-    ],
-  },
-  {
-    label: "View",
-    submenu: [
-      { role: "reload" },
-      { role: "forcereload" },
-      { role: "toggledevtools" },
-      { type: "separator" },
-      { role: "resetzoom" },
-      { role: "zoomin" },
-      { role: "zoomout" },
-      { type: "separator" },
-      { role: "togglefullscreen" },
-    ],
-  },
-  {
-    role: "window",
-    submenu: [{ role: "minimize" }, { role: "close" }],
-  },
-];
-
-if (process.platform === "darwin") {
-  template.unshift({
-    label: app.name,
-    submenu: [
-      { role: "about" },
-      { type: "separator" },
-      { role: "services", submenu: [] },
-      { type: "separator" },
-      { role: "hide" },
-      { role: "hideothers" },
-      { role: "unhide" },
-      { type: "separator" },
-      { role: "quit" },
-    ],
-  });
-
-  // Edit menu
-  template[1].submenu.push(
-    { type: "separator" },
-    {
-      label: "Speech",
-      submenu: [{ role: "startspeaking" }, { role: "stopspeaking" }],
-    }
-  );
-
-  // Window menu
-  template[3].submenu = [
-    { role: "close" },
-    { role: "minimize" },
-    { role: "zoom" },
-    { type: "separator" },
-    { role: "front" },
-  ];
-}
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -125,15 +51,36 @@ app.on("ready", () => {
   // Display Dev Tools
   // mainWindow.openDevTools();
 
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
-});
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
-  data = {
-    bounds: mainWindow.getBounds(),
-  };
-  fs.writeFileSync(initPath, JSON.stringify(data));
-  app.quit();
+// app.on("window-all-closed", () => {
+//   data = {
+//     bounds: mainWindow.getBounds(),
+//   };
+//   fs.writeFileSync(initPath, JSON.stringify(data));
+//   app.quit();
+// });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  if (process.platform === 'darwin') {
+    var forceQuit = false;
+    app.on('before-quit', function() {
+      forceQuit = true;
+    });
+    mainWindow.on('close', function(event) {
+      if (!forceQuit) {
+        event.preventDefault();
+        mainWindow.hide();
+      }
+    });
+  }
+
 });
+
+app.on('activate', () => mainWindow.show());
+
+app.dock.setBadge("•")
+// app.setBadgeCount(3)
